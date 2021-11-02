ff7c19cb34f183bc8467b2adb22f340db9c5b46c
Ezekiel Carvalho <ezekielcarvalho@ymail.com>
2021-11-02 23:47:13 +0530

    updated settings file

--- costas/settings.py
+++ costas/settings.py
@@ -67,9 +67,11 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
